de0d8ffeb942d339d03ebe2da238b1fef3bd489d
Manoj Kumal <manoj.kumal152@gmail.com>
2022-10-15 12:04:20 +0545

    quicksort file added

---
+++ sorting/quicksort.py
@@ -0,0 +1,35 @@
+# Implementation of QuickSort
+
+def swap(a,b,arr):
+    if a!=b:
+        tmp = arr[a]
+        arr[a] = arr[b]
+        arr[b] = tmp
+
+def Partition(arr,start,end):
+    pivot_index = start
+    pivot = arr[pivot_index]
+    while start < end:
+        while start < len(arr) and arr[start] <= pivot:
+            start += 1
+        while arr[end] > pivot:
+            end -= 1
+        if start<end:
+            swap(start, end, arr)
+    swap(pivot_index, end, arr)
+    return end
+
+def quick_sort(arr,start,end):
+    if start < end:
+        pi = Partition(arr,start,end) # partition index
+        quick_sort(arr,start,pi-1) # the left partition
+        quick_sort(arr,pi+1,end) # the right partition
+        return arr
+
+# driver code
+arr = [11, 9, 29, 7, 2, 15, 28]
+start = 0
+end = len(arr) - 1
+# function call
+result = quick_sort(arr,start,end)
+print(f"The Sorted Array is {result} ")
